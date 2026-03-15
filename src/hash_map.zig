921b3f4a1567dc9fe0864ad9c880f9994cffc2d5
Santtu Niskanen <mail@salamanteri.pro>
2026-03-15 21:18:07 +0200

    add set function

--- src/hash_map.zig
+++ src/hash_map.zig
@@ -5,6 +5,17 @@
 //! a fixed size. It's not really an actual HashMap yet.
 const std = @import("std");
 
+/// this is basically the item we store in the HashMap
+/// struct. It just holds an optional key and value.
+///
+/// Having both values optional feels kinda dumb, but I'm also
+/// assigning just a null value to them when initializing
+/// the HashMap struct, so it's whatever for now...
+const Item = struct {
+    key: ?[]const u8,
+    value: ?[]const u8,
+};
+
 /// The HashMap struct currently just holds a single bucket of items
 /// that has a set size. There's no need to do any memory allocations
 /// right now, but this should obviously have a dynamic size later.
@@ -15,21 +26,24 @@ const HashMap = struct {
     pub fn init() HashMap {
         return HashMap{ .items = [_]?Item{null} ** 16 };
     }
-};
 
-/// this is basically the item we store in the HashMap
-/// struct. It just holds an optional key and value.
-///
-/// Having both values optional feels kinda dumb, but I'm also
-/// assigning just a null value to them when initializing
-/// the HashMap struct, so it's whatever for now...
-const Item = struct {
-    key: ?[]const u32,
-    value: ?[]const u32,
+    /// this currently doesn't work as it's supposed to, but I just want
+    /// to set values.
+    ///
+    /// we take a pointer to the map, then manually add an index and then give the
+    /// function two string literals as the key and value. It then replaces the
+    /// Item in the HashMap in the appropriate index. Not really how a HashMap
+    /// is supposed to work, but we'll get the hashing done eventually.
+    pub fn set(s: *HashMap, index: u8, key: []const u8, value: []const u8) void {
+        s.items[index] = Item{ .key = key, .value = value };
+    }
 };
 
 test "Create HashMap and print items..." {
-    const s = HashMap.init();
+    var s = HashMap.init();
+
+    HashMap.set(&s, 1, "key", "value");
+    HashMap.set(&s, 4, "other", "value");
 
     for (0..s.items.len) |i| {
         std.debug.print("item: {any}; {any}\n", .{ i, s.items[i] });
